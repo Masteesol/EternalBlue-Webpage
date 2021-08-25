656c6fc229e7f65d99b2d6c02f9b3b13215b09ec
Masteesol <marius.steen.solheim@outlook.com>
2021-08-25 17:46:43 +0200

    js.update

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const nav = document.querySelector("nav");
 const lyricsContainer = document.querySelector(".lyricscontainer");
 const rightColumn = document.querySelector(".rightcolumn");
 const main = document.querySelector("main")
-const songListNav = document.querySelectorAll("ol li button")
+const songListNav = document.querySelectorAll(".song")
 const headerLyrics = document.querySelector(".headerlyrics")
-
+const navBar = document.querySelector("#navbar")
 
 releasesBtn.addEventListener("click", function (e) {
     releases.style.display = "flex";
@@ -21,31 +21,48 @@ releasesBtn.addEventListener("click", function (e) {
 
 aboutBtn.addEventListener("click", function (e) {
     releases.style.display = "none";
-    about.style.display = "flex";
+    about.style.display = "block";
     aboutBtn.style.textDecoration = "underline"
     releasesBtn.style.textDecoration = "none"
     lyricsContainer.style.display ="none"
 })
 
 viewBtn.addEventListener("click", function (e) {
+    addNavButton()
     nav.style.display = "none";
     lyricsContainer.style.display = "flex";
     rightColumn.style.display = "none";
     viewBtn.style.display ="none";
     exitView.style.display = "inline"
-    songListNav.forEach(item => item.classList.add("song"))
+    songListNav.forEach(item => item.classList.remove("song"))
+    songListNav.forEach(item => item.classList.add("song-mod"))
     headerLyrics.style.display = "inline"
+    navBar.classList.add("navbaractive")
+    openNavFunction()
 })
 
+function addNavButton () {
+    const button = document.createElement("button");
+    button.classList.add("opennav");
+    const listingContainer = document.querySelector(".listingcontainer")
+    main.insertBefore(button, listingContainer);
+}
+
+
 exitView.addEventListener("click", function (e) {
+    navBar.removeAttribute("class")
+    navBar.removeAttribute("style")
     nav.style.display = "inline";
     lyricsContainer.style.display = "none";
     rightColumn.style.display = "flex";
     viewBtn.style.display ="block";
     exitView.style.display = "none"
     headerLyrics.style.display = "none"
-    songListNav.forEach(item => item.classList.remove("song"))
+    songListNav.forEach(item => item.classList.remove("song-mod"))
+    songListNav.forEach(item => item.classList.add("song"))
+    document.querySelector(".opennav").remove()
 })
+
 
 main.addEventListener("click",function (e) {
     console.log(e)
@@ -57,3 +74,28 @@ main.addEventListener("click",function (e) {
     scrollToElm.scrollIntoView({behavior: "smooth"})
     }
 })
+
+function openNavFunction () {
+    const openNavBtn = document.querySelector(".opennav")
+    let toggled = 0;
+    openNavBtn.addEventListener("click", function(e) {
+        if (toggled === 0) { 
+            openNavBtn.style.cssText =`
+                                        background-image: url(images/times-solid.svg);
+                                        top: 20px;
+                                        left: 200px;
+                                        `;
+            document.querySelector("#navbar").style.width = "250px";
+            toggled = 1;
+        } else {
+            openNavBtn.style.cssText =`
+                                        background-image: url(images/bars-solid.svg);
+                                        left: 20px;
+                                        `;  
+            document.querySelector("#navbar").style.width = "0";
+            toggled = 0;
+        }
+    })
+}
+
+
